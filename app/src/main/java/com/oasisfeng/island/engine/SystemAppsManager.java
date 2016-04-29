a230d3ddb94fd3dcc92387aeaeafc44197f9b9ff
oasisfeng <git@id.oasisfeng.com>
2016-04-29 22:31:22 +0800

    UPDATE: Refine the white-list of known critical system packages, with web browser app removed from the white-list. (web URL request is forwarded)

--- app/src/main/java/com/oasisfeng/island/engine/SystemAppsManager.java
+++ app/src/main/java/com/oasisfeng/island/engine/SystemAppsManager.java
@@ -8,13 +8,13 @@ import android.content.pm.PackageManager;
 import android.content.pm.ProviderInfo;
 import android.content.pm.ResolveInfo;
 import android.content.pm.ServiceInfo;
-import android.net.Uri;
 import android.provider.CalendarContract;
 import android.provider.CallLog;
 import android.provider.ContactsContract;
+import android.provider.ContactsContract.Contacts;
 import android.provider.MediaStore;
 import android.provider.Settings;
-import android.provider.Telephony;
+import android.provider.Telephony.Carriers;
 import android.util.Log;
 
 import com.google.common.collect.FluentIterable;
@@ -42,13 +42,16 @@ public class SystemAppsManager {
 	public static final String PACKAGE_GOOGLE_PLAY_SERVICES = "com.google.android.gms";
 	public static final String PACKAGE_GOOGLE_PLAY_STORE = "com.android.vending";
 
+	/** This list serves as a known common package names for quick filtering */
 	private static final Collection<String> sCriticalSystemPkgs = Arrays.asList(
 			"android", "com.android.systemui",		// There packages are generally safe to either freeze or not, leave them unfrozen for better compatibility.
 			"com.android.packageinstaller",			// Package installer responsible for ACTION_INSTALL_PACKAGE (AOSP)
 			"com.android.settings",					// For various setting intent activities
 			"com.android.keychain",					// MIUI system will crash without this
 			"com.android.providers.telephony",		// The SMS & MMS and carrier info provider. (SMS & MMS provider is shared across all users)
-			"com.android.providers.contacts",		// The storage of contacts, for Contacts app to work
+			"com.android.providers.contacts",		// The storage of contacts, for contacts data access.
+			"com.android.providers.calllogbackup",	// The storage of call log (deprecated)
+			"com.android.providers.calendar",		// The storage of calendar, for calendar data access.
 			"com.android.providers.downloads",		// For DownloadManager to work
 			"com.android.providers.media",			// For media access
 			"com.android.externalstorage",			// Documents provider - Internal storage
@@ -56,16 +59,17 @@ public class SystemAppsManager {
 			"com.android.webview",					// WebView provider (AOSP)
 			"com.android.vpndialogs",				// Required by VPN service app (e.g. ShadowSocks)
 			"com.android.documentsui",				// Document picker
-			// Google packages
+			// Enabled system apps with launcher activity by default
+			PACKAGE_GOOGLE_PLAY_STORE,				// Google Play Store to let user install apps directly within
+			"com.android.contacts",					// Contacts
+			"com.android.providers.downloads.ui",	// Downloads
+			// Essential Google packages
 			"com.google.android.gsf",				// Google services framework
 			"com.google.android.packageinstaller",	// Package installer (Google)
 			PACKAGE_GOOGLE_PLAY_SERVICES,			// Disabling GMS in the provision will cause GMS in owner user being killed too due to its single user nature, causing weird ANR.
 			"com.google.android.feedback",			// Used by GMS for crash report
 			"com.google.android.webview",			// WebView provider (Google)
-			// Enabled system apps with launcher activity by default
-			PACKAGE_GOOGLE_PLAY_STORE,				// Google Play Store to let user install apps directly within
-			"com.android.contacts",					// Contacts
-			"com.android.providers.downloads.ui",	// Downloads
+			"com.google.android.contacts",			// Contacts (Google)
 			// MIUI-specific
 			"com.miui.core"							// Required by com.lbe.security.miui (Runtime permission UI of MIUI)
 	);
@@ -73,21 +77,24 @@ public class SystemAppsManager {
 			new Intent(Intent.ACTION_INSTALL_PACKAGE),				// Usually com.android.packageinstaller, may be altered by ROM.
 			new Intent(Settings.ACTION_SETTINGS),					// Usually com.android.settings
 			new Intent("android.content.pm.action.REQUEST_PERMISSIONS"),	// Hidden PackageManager.ACTION_REQUEST_PERMISSIONS
-			// Runtime permission UI, may be special system app on some ROMs (e.g. MIUI)
+																	// Runtime permission UI, may be special system app on some ROMs (e.g. MIUI)
 			new Intent(Intent.ACTION_OPEN_DOCUMENT).setType("*/*"),	// Usually com.android.documentsui, may be file explorer app on some ROMs. (e.g. MIUI)
 			new Intent(Intent.ACTION_CREATE_DOCUMENT).setType("*/*"),
-			new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI),	// Contact picker, usually com.android.contacts
-			new Intent(Intent.ACTION_VIEW, Uri.parse("http://g.cn"))// Web browser (required by some apps to open web link)
+			new Intent(Intent.ACTION_PICK, Contacts.CONTENT_URI)	// Contact picker, usually com.android.contacts
 	);
 	private static final Collection<String> sCriticalContentAuthorities = Arrays.asList(
 			ContactsContract.AUTHORITY,				// Usually com.android.providers.contacts
-			CalendarContract.AUTHORITY,				// Usually com.android.providers.calendar
 			CallLog.AUTHORITY,						// Usually com.android.providers.contacts (originally com.android.providers.calllogbackup)
-			Telephony.Carriers.CONTENT_URI.getAuthority(),	// Usually com.android.providers.telephony
+			CalendarContract.AUTHORITY,				// Usually com.android.providers.calendar
+			Carriers.CONTENT_URI.getAuthority(),	// Usually com.android.providers.telephony
 			MediaStore.AUTHORITY,					// Usually com.android.providers.media
 			"downloads",							// Usually com.android.providers.downloads
 			"com.android.externalstorage.documents" // Usually com.android.externalstorage
 	);
+
+	public boolean isCritical(final String pkg) {
+		return sCriticalSystemPkgs.contains(pkg);	// TODO: Intent-based matching on sCriticalActivityIntents
+	}
 
 	public void prepareSystemApps() {
 		disableNonCriticalSystemApps();
