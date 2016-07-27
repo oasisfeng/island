9807a23400a180e013634ce4b063004ea92eb3c2
oasisfeng <git@id.oasisfeng.com>
2016-07-27 16:35:39 +0800

    FIX: Avoid import class from indirectly dependent Apache Commons Codec library in ApiTokenManager.

--- app/src/main/java/com/oasisfeng/island/api/ApiTokenManager.java
+++ app/src/main/java/com/oasisfeng/island/api/ApiTokenManager.java
@@ -3,8 +3,6 @@ package com.oasisfeng.island.api;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-import org.apache.commons.codec.binary.Hex;
-
 import java.security.SecureRandom;
 
 /**
@@ -15,6 +13,7 @@ import java.security.SecureRandom;
 public class ApiTokenManager {
 
 	private static final String PREFS_NAME_API_TOKENS = "api-tokens";
+	private static final char[] DIGITS_UPPER = new char[] {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
 	public ApiTokenManager(final Context context) {
 		mTokenStore = context.getSharedPreferences(PREFS_NAME_API_TOKENS, Context.MODE_PRIVATE);
@@ -23,9 +22,19 @@ public class ApiTokenManager {
 	public String getToken(final String pkg) {
 		final String token = mTokenStore.getString(pkg, null);
 		if (token != null) return token;
-		final String new_token = Hex.encodeHexString(SecureRandom.getSeed(8)).toUpperCase();
+		final String new_token = generateToken();
 		mTokenStore.edit().putString(pkg, new_token).apply();	// TODO: Potential failure in persistence, consider pre-allocation.
 		return new_token;
+	}
+
+	private String generateToken() {
+		final byte[] data = SecureRandom.getSeed(8);
+		final char[] token = new char[data.length << 1];
+		for (int i = 0, j = 0; i < data.length; i ++) {
+			token[j ++] = DIGITS_UPPER[(240 & data[i]) >>> 4];
+			token[j ++] = DIGITS_UPPER[15 & data[i]];
+		}
+		return new String(token);
 	}
 
 	boolean verifyToken(final String token) {
