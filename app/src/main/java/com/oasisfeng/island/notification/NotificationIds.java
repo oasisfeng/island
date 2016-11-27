ff30df13e2080f8c6253416a5729e27310e223ce
oasisfeng <git@id.oasisfeng.com>
2016-11-28 01:04:24 +0800

    UPDATE: Start foreground service in ShuttleKeep to survive extreme low memory situation.

---
+++ app/src/main/java/com/oasisfeng/island/notification/NotificationIds.java
@@ -0,0 +1,11 @@
+package com.oasisfeng.island.notification;
+
+/**
+ * Central definition for all notification IDs, to avoid conflicts.
+ *
+ * Created by Oasis on 2016/11/28.
+ */
+public class NotificationIds {
+
+	public static final int SHUTTLE_KEEPER = 1;
+}
--- app/src/main/java/com/oasisfeng/island/shuttle/ShuttleKeeper.java
+++ app/src/main/java/com/oasisfeng/island/shuttle/ShuttleKeeper.java
@@ -1,6 +1,7 @@
 package com.oasisfeng.island.shuttle;
 
 import android.app.ActivityManager;
+import android.app.Notification;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -9,7 +10,16 @@ import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
+import com.oasisfeng.island.R;
+import com.oasisfeng.island.notification.NotificationIds;
+
 import java.util.List;
+
+import static android.app.Notification.PRIORITY_MIN;
+import static android.os.Build.VERSION.SDK_INT;
+import static android.os.Build.VERSION_CODES.N;
 
 /**
  * Keep the process from being destroyed when service shuttle is working
@@ -38,11 +48,31 @@ public class ShuttleKeeper extends Service {
 	}
 
 	@Override public int onStartCommand(final Intent intent, final int flags, final int startId) {
-		return START_NOT_STICKY;		// STICKY has no point here, since we share process with caller.
+		startForeground();				// To avoid shuttle service being killed.
+		return START_NOT_STICKY;		// STICKY has no point here, since we share process with the direct service client (not the client in owner user).
 	}
 
-	@Override public void onCreate() { Log.d(TAG, "Start"); }
-	@Override public void onDestroy() { Log.d(TAG, "Stop"); }
+	@Override public void onTrimMemory(final int level) {
+		if (level > TRIM_MEMORY_UI_HIDDEN) Log.i(TAG, "onTrimMemory: " + level);	// TRIM_MEMORY_UI_HIDDEN is always triggered since the shuttle is activity (UI).
+	}
+
+	private void startForeground() {
+		startForeground(NotificationIds.SHUTTLE_KEEPER, mForegroundNotification.get());
+	}
+
+	@Override public void onCreate() {
+		Log.d(TAG, "Start");
+		mForegroundNotification = Suppliers.memoize(() ->
+				new Notification.Builder(this).setSmallIcon(android.R.drawable.stat_notify_sync_noanim).setPriority(PRIORITY_MIN)
+						.setContentTitle(SDK_INT >= N ? null : getString(R.string.app_name)).setSubText(getString(R.string.notification_standby_text)).build());
+	}
+
+	@Override public void onDestroy() {
+		stopForeground(true);
+		Log.d(TAG, "Stop");
+	}
+
+	private Supplier<Notification> mForegroundNotification;
 
 	private static final BroadcastReceiver DUMMY_RECEIVER = new BroadcastReceiver() { @Override public void onReceive(final Context c, final Intent i) {}};
 	private static final String TAG = "ShuttleKeeper";
