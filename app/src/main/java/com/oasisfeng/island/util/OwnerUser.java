2084a3c66e6a23fae5d87f757606c87c12b8e63e
oasisfeng <git@id.oasisfeng.com>
2016-11-27 17:53:29 +0800

    UPDATE: Add two new annotation @OwnerUser and @ProfileUser to indicate the user space in which class/method is running.

---
+++ app/src/main/java/com/oasisfeng/island/util/OwnerUser.java
@@ -0,0 +1,18 @@
+package com.oasisfeng.island.util;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.SOURCE;
+
+/**
+ * Annotation to indicate running in owner user.
+ *
+ * Created by Oasis on 2016/11/27.
+ */
+@Retention(SOURCE)
+@Target({TYPE,METHOD,CONSTRUCTOR})
+public @interface OwnerUser {}
