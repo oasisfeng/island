6c3bfccefb156f10513ec6eef0ac2a2be12a7e1d
oasisfeng <git@id.oasisfeng.com>
2017-01-27 00:57:14 +0800

    UPDATE: Support both debug and release version of Greenify in debug build of Island.

--- app/src/main/java/com/oasisfeng/island/greenify/GreenifyClient.java
+++ app/src/main/java/com/oasisfeng/island/greenify/GreenifyClient.java
@@ -11,6 +11,7 @@ import android.os.UserHandle;
 import android.os.UserManager;
 import android.support.annotation.Nullable;
 
+import com.oasisfeng.android.util.Apps;
 import com.oasisfeng.island.BuildConfig;
 
 import static android.content.Context.USER_SERVICE;
@@ -22,14 +23,13 @@ import static android.content.Context.USER_SERVICE;
  */
 public class GreenifyClient {
 
-	private static final String GREENIFY_PKG = BuildConfig.DEBUG ? "com.oasisfeng.greenify.debug" : "com.oasisfeng.greenify";
 	private static final String GREENIFY_ACTION = "com.oasisfeng.greenify.action.GREENIFY";
 	private static final int MIN_GREENIFY_VERSION = BuildConfig.DEBUG ? 218 : 219;	// TODO: The minimal version of Greenify with support for Island.
 
 	public static boolean greenify(final Activity activity, final String pkg, final UserHandle user) {
 		final long user_sn = ((UserManager) activity.getSystemService(USER_SERVICE)).getSerialNumberForUser(user);
 		if (user_sn == -1) return false;
-		final Intent intent = new Intent(GREENIFY_ACTION).setPackage(GREENIFY_PKG).setData(Uri.parse("package:" + pkg + "#usn=" + user_sn));
+		final Intent intent = new Intent(GREENIFY_ACTION).setPackage(getGreenifyPackage(activity)).setData(Uri.parse("package:" + pkg + "#usn=" + user_sn));
 		try {
 			activity.startActivityForResult(intent, 0);
 		} catch (final ActivityNotFoundException e) {
@@ -40,7 +40,7 @@ public class GreenifyClient {
 
 	public static @Nullable Boolean checkGreenifyVersion(final Context context) {
 		try { @SuppressWarnings("deprecation")
-			final PackageInfo greenify_info = context.getPackageManager().getPackageInfo(GREENIFY_PKG, PackageManager.GET_UNINSTALLED_PACKAGES);
+			final PackageInfo greenify_info = context.getPackageManager().getPackageInfo(getGreenifyPackage(context), PackageManager.GET_UNINSTALLED_PACKAGES);
 			return greenify_info.versionCode >= MIN_GREENIFY_VERSION;
 		} catch (final PackageManager.NameNotFoundException e) {
 			return null;
@@ -48,6 +48,24 @@ public class GreenifyClient {
 	}
 
 	public static void openInAppMarket(final Activity activity) {
-		activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + GREENIFY_PKG)));
+		try {
+			activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + GREENIFY_PKG)));
+		} catch (final ActivityNotFoundException e) {
+			try {
+				activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + GREENIFY_PKG)));
+			} catch (final ActivityNotFoundException ignored) {}
+		}
 	}
+
+	private static String getGreenifyPackage(final Context context) {
+		if (sGreenifyPackageName == null) {
+			if (BuildConfig.DEBUG && Apps.of(context).isInstalledInCurrentUser(GREENIFY_DEBUG_PKG)) sGreenifyPackageName = GREENIFY_DEBUG_PKG;
+			else sGreenifyPackageName = GREENIFY_PKG;
+		}
+		return sGreenifyPackageName;
+	}
+
+	private static String sGreenifyPackageName;
+	private static final String GREENIFY_PKG = "com.oasisfeng.greenify";
+	private static final String GREENIFY_DEBUG_PKG = "com.oasisfeng.greenify.debug";
 }
