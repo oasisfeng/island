0efe308bef927064123aa5066b131fc75a9b1af7
oasisfeng <git@id.oasisfeng.com>
2017-03-10 01:01:54 +0800

    ADD: AdvancedPreference and ActionButtonPreference.

---
+++ app/src/main/java/com/oasisfeng/settings/ActionButtonPreference.java
@@ -0,0 +1,47 @@
+package com.oasisfeng.settings;
+
+import android.content.Context;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.StringRes;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.oasisfeng.island.mobile.R;
+
+/** A preference with a clickable action icon on the side */
+public class ActionButtonPreference extends AdvancedPreference implements View.OnClickListener {
+
+	public void setSummaryAndActionButton(final @StringRes int summary, final @DrawableRes int icon, final OnPreferenceClickListener listener) {
+		setSummaryAndNotSelectable(summary);
+		mActionIcon = icon;
+		mOnActionClickListener = listener;
+		notifyChanged();
+	}
+
+	@Override protected void onBindView(final View view) {
+		super.onBindView(view);
+		final ImageView icon = (ImageView) view.findViewById(R.id.preference_action_button);
+		if (mActionIcon != 0) {
+			icon.setImageDrawable(getContext().getDrawable(mActionIcon));
+			icon.setOnClickListener(this);
+			icon.setEnabled(true);		// Make icon available even if the preference itself is disabled.
+			icon.setVisibility(View.VISIBLE);
+		} else icon.setVisibility(View.GONE);
+	}
+
+	@Override public void onClick(final View v) {
+		if (v.getId() == R.id.preference_action_button)
+			if (mOnActionClickListener != null)
+				mOnActionClickListener.onPreferenceClick(this);
+	}
+
+	public ActionButtonPreference(final Context context, final AttributeSet attrs) {
+		super(context, attrs);
+		mActionIcon = attrs.getAttributeResourceValue("http://schemas.android.com/apk/res-auto", "icon", 0);
+		setWidgetLayoutResource(R.layout.preference_widget_action_button);
+	}
+
+	private @DrawableRes int mActionIcon;
+	private OnPreferenceClickListener mOnActionClickListener;
+}
---
+++ app/src/main/java/com/oasisfeng/settings/AdvancedPreference.java
@@ -0,0 +1,42 @@
+package com.oasisfeng.settings;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.preference.Preference;
+import android.support.annotation.StringRes;
+import android.util.AttributeSet;
+
+/**
+ * Improved {@link Preference}
+ *
+ * Created by Oasis on 2017/3/7.
+ */
+public class AdvancedPreference extends Preference {
+
+	private static final String PACKAGE_PLACEHOLDER_SELF = "self";
+
+	@Override public void setIntent(final Intent intent) {
+		final ComponentName component = intent.getComponent();
+		if (component != null && PACKAGE_PLACEHOLDER_SELF.equals(component.getPackageName())) {
+			if (component.getClassName().isEmpty()) intent.setPackage(getContext().getPackageName()).setComponent(null);
+			else intent.setComponent(new ComponentName(getContext().getPackageName(), component.getClassName()));
+		}
+		super.setIntent(intent);
+	}
+
+	public void setSummaryAndNotSelectable(final @StringRes int summary) {
+		setSummary(summary);
+		setSelectable(false);
+	}
+
+	public void setSummaryAndClickListener(final @StringRes int summary, final OnPreferenceClickListener listener) {
+		setSummary(summary);
+		setSelectable(true);
+		setOnPreferenceClickListener(listener);
+	}
+
+	public AdvancedPreference(final Context context, final AttributeSet attrs) {
+		super(context, attrs);
+	}
+}
