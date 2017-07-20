9680f78ca1321d1f5e2f33049fce54c3bdeab1c5
oasisfeng <git@id.oasisfeng.com>
2017-07-20 14:50:04 +0800

    UPDATE: Re-provision device owner after Island is updated.

--- engine/src/main/java/com/oasisfeng/island/InternalReceiver.java
+++ engine/src/main/java/com/oasisfeng/island/InternalReceiver.java
@@ -8,4 +8,5 @@ package com.oasisfeng.island;
 public class InternalReceiver {
 
 	public static final class _1 extends com.oasisfeng.island.provisioning.ManualProvisioningReceiver {}
+	public static final class _2 extends com.oasisfeng.island.AppUpdateReceiver {}
 }
