60f1db942e878cd993e8edfaf3cfb5a3e7f4db7b
oasisfeng <git@id.oasisfeng.com>
2017-07-22 11:17:43 +0800

    FIX: Unexpected SecurityException in DeviceAdmins.getComponentName().

--- shared/src/main/java/com/oasisfeng/island/util/DeviceAdmins.java
+++ shared/src/main/java/com/oasisfeng/island/util/DeviceAdmins.java
@@ -1,10 +1,13 @@
 package com.oasisfeng.island.util;
 
 import android.app.admin.DeviceAdminReceiver;
+import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ResolveInfo;
+
+import com.oasisfeng.island.analytics.Analytics;
 
 import java.util.List;
 
@@ -16,12 +19,27 @@ import java.util.List;
 public class DeviceAdmins {
 
 	public static ComponentName getComponentName(final Context context) {
-		if (sDeviceAdminComponent != null) return sDeviceAdminComponent;
-		final List<ResolveInfo> admins = context.getPackageManager().queryBroadcastReceivers(
-				new Intent(DeviceAdminReceiver.ACTION_DEVICE_ADMIN_ENABLED).setPackage(Modules.MODULE_ENGINE), 0);
-		if (admins.size() != 1) throw new IllegalStateException("Engine module is not correctly installed.");
-		return sDeviceAdminComponent = new ComponentName(Modules.MODULE_ENGINE, admins.get(0).activityInfo.name);
+		if (sDeviceAdminComponent == null) sDeviceAdminComponent = queryComponentName(context);
+		return sDeviceAdminComponent;
+	}
+
+	private static ComponentName queryComponentName(final Context context) {
+		final List<ComponentName> active_admins = ((DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE)).getActiveAdmins();
+		if (active_admins != null && ! active_admins.isEmpty()) for (final ComponentName active_admin : active_admins)
+			if (Modules.MODULE_ENGINE.equals(active_admin.getPackageName())) return active_admin;
+
+		try {
+			final List<ResolveInfo> admins = context.getPackageManager().queryBroadcastReceivers(
+					new Intent(DeviceAdminReceiver.ACTION_DEVICE_ADMIN_ENABLED).setPackage(Modules.MODULE_ENGINE), 0);
+			if (admins.size() != 1) throw new IllegalStateException("Engine module is not correctly installed.");
+			return sDeviceAdminComponent = new ComponentName(Modules.MODULE_ENGINE, admins.get(0).activityInfo.name);
+		} catch (final SecurityException e) {
+			Analytics.$().report(e);
+			return new ComponentName(Modules.MODULE_ENGINE, "com.oasisfeng.island.IslandDeviceAdminReceiver");	// Fallback
+		}
 	}
 
 	private static ComponentName sDeviceAdminComponent;
+
+	private static final String TAG = DeviceAdmins.class.getSimpleName();
 }
