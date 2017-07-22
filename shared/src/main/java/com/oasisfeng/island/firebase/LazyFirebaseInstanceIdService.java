064ee90a6b7a931065b008363fe64e578644f115
oasisfeng <git@id.oasisfeng.com>
2017-07-22 10:28:11 +0800

    FIX: Random crash in FirebaseInstanceIdService caused by lazy initialization of Firebase.

---
+++ shared/src/main/java/com/oasisfeng/island/firebase/LazyFirebaseInstanceIdService.java
@@ -0,0 +1,17 @@
+package com.oasisfeng.island.firebase;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Wrapper of {@link FirebaseInstanceIdService} to ensure Firebase is lazily initialized first.
+ *
+ * Created by Oasis on 2017/7/22.
+ */
+public class LazyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+
+	@Override public void onCreate() {
+		super.onCreate();
+		FirebaseApp.initializeApp(this);
+	}
+}
