oasisfeng/island

commit 8afa303ada1e13c76040dae003bd1352b7665803

UPDATE: Add explicit consent for device-admin privilege in setup guide.
As required by Google Play developer policy.

app/src/main/java/com/oasisfeng/island/setup/SetupWizardFragment.java
@@ -31,14 +31,15 @@ public class SetupWizardFragment extends Fragment implements NavigationBar.Navig
 		} else {
 			final Bundle args = getArguments();
 			final SetupViewModel vm = args != null ? args.getParcelable(null) : null;
-			mViewModel = vm != null ? vm : new SetupViewModel();
+			mViewModel = vm != null ? vm : new SetupViewModel();	// Initial view - "Welcome"
+			mViewModel.button_next = R.string.setup_accept;			// "Accept" button for device-admin privilege consent, required by Google Play developer policy.
 		}
 
 		mContainerViewId = container.getId();
 		final SetupWizardBinding binding = SetupWizardBinding.inflate(inflater, container, false);
 		binding.setSetup(mViewModel);
 		final View view = binding.getRoot();
-		final SetupWizardLayout layout = (SetupWizardLayout) view.findViewById(R.id.setup_wizard_layout);
+		final SetupWizardLayout layout = view.findViewById(R.id.setup_wizard_layout);
 		layout.requireScrollToBottom();
 
 		final NavigationBar nav_bar = layout.getNavigationBar();

app/src/main/res/values/strings.xml
@@ -12,10 +12,15 @@
     <string name="status_appendix_exclusive">Exclusive</string>
 
     <string name="setup_profile_welcome">Welcome</string>
-    <string name="setup_island_intro">\"Island\" is a sandbox environment to clone selected apps and isolate them from accessing your personal data outside (including call logs, contacts and etc) even if permissions are granted.\n\nCloned app runs parallel aside from the installed one with its separate copy of app data. It can also be frozen on demand, with launcher icon vanished and its background behaviors completely blocked.</string>
+    <string name="setup_island_intro">\"Island\" is a sandbox environment to clone selected apps and isolate them from accessing your personal data
+outside (including call logs, contacts and etc) even if permissions are granted.
+\n\nCloned app runs parallel aside from the installed one with its separate copy of app data. It can also be frozen on demand, with launcher icon
+vanished and its background behaviors completely blocked.
+\n\nBy creating the sandbox environment, <b>you authorize Island to act as device administrator of \"Android for Work\", with the privilege to manage
+the sandbox environment</b>. Island never collect data related to your privacy, please read our privacy policy on Google Play Store for more details.</string>
     <string name="setup_profile_conflict">Android for Work is already set up on your device by \"%1$s\". Due to the limitation of Android system, Island cannot coexist with it. If you proceed, the existent profile WITH ALL ITS DATA WILL BE REMOVED.</string>
     <string name="setup_profile_owner_unknown_app">unknown app</string>
-    <string name="setup_button">Build the Island now</string>
+    <string name="setup_accept">Accept</string>
     <string name="setup_cleanup_hint">If anything goes wrong during the setup procedure, you can always remove all the data created by Island via \"Settings - Account - Remove work profile\".</string>
     <string name="error_rom_incompatible">Sorry, your device (or ROM) is incompatible with Island.\n\n> %s\n\nPlease refer to the online FAQ for possible solutions.</string>
     <string name="setup_error_managed_profile_not_supported">Sorry, the required feature \"Managed Profile\" is not supported on this device.</string>

commit 6d87bb4be87cb383665f5543c23c92d406209683

FIX: "Accept" button is incorrectly showing elsewhere besides Welcome in setup wizard.
This bug was introduced by commit 8afa303ada1e13c76040dae003bd1352b7665803.

app/src/main/java/com/oasisfeng/island/setup/SetupWizardFragment.java
@@ -21,8 +21,6 @@ import com.oasisfeng.island.mobile.databinding.SetupWizardBinding;
  */
 public class SetupWizardFragment extends Fragment implements NavigationBar.NavigationBarListener {
 
-	// Tag for creating this fragment. This tag can be used to retrieve this fragment.
-	public static final String FRAGMENT_TAG = "SetupManagementFragment";
 	private static final String EXTRA_VIEW_MODEL = "vm";
 
 	@Override public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
@@ -31,8 +29,10 @@ public class SetupWizardFragment extends Fragment implements NavigationBar.Navig
 		} else {
 			final Bundle args = getArguments();
 			final SetupViewModel vm = args != null ? args.getParcelable(null) : null;
-			mViewModel = vm != null ? vm : new SetupViewModel();	// Initial view - "Welcome"
-			mViewModel.button_next = R.string.setup_accept;			// "Accept" button for device-admin privilege consent, required by Google Play developer policy.
+			if (vm == null) {
+				mViewModel = new SetupViewModel();    			// Initial view - "Welcome"
+				mViewModel.button_next = R.string.setup_accept;	// "Accept" button for device-admin privilege consent, required by Google Play developer policy.
+			} else mViewModel = vm;
 		}
 
 		mContainerViewId = container.getId();