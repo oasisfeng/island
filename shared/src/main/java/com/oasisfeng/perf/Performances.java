e045e7be09ca38958f3718b1d7ddcd8b413bfee4
oasisfeng <git@id.oasisfeng.com>
2017-09-11 20:32:56 +0800

    UPDATE: Add missing utility class Performances.

---
+++ shared/src/main/java/com/oasisfeng/perf/Performances.java
@@ -0,0 +1,51 @@
+package com.oasisfeng.perf;
+
+import android.os.SystemClock;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.google.common.base.Stopwatch;
+import com.google.common.base.Ticker;
+import com.oasisfeng.deagle.BuildConfig;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Utility class for performance related stuffs
+ *
+ * Created by Oasis on 2016/9/25.
+ */
+public class Performances {
+
+	private static final boolean DEBUG = BuildConfig.DEBUG;
+
+	public static final class Debug {
+
+		public static @Nullable Stopwatch startUptimeStopwatch() {
+			return DEBUG ? Stopwatch.createStarted(TICKER_UPTIME) : null;
+		}
+
+		public static @Nullable Stopwatch startThreadCpuTimeStopwatch() {
+			return DEBUG ? Stopwatch.createStarted(TICKER_THREAD_CPU_TIME) : null;
+		}
+	}
+
+	public static final Ticker TICKER_UPTIME = new Ticker() { @Override public long read() { return SystemClock.uptimeMillis() * 1_000_000L; }};
+	public static final Ticker TICKER_THREAD_CPU_TIME = new Ticker() { @Override public long read() { return android.os.Debug.threadCpuTimeNanos(); }};
+
+	public static @Nullable Stopwatch startUptimeStopwatch() {
+		return Stopwatch.createStarted(TICKER_UPTIME);
+	}
+
+	public static @Nullable Stopwatch startThreadCpuTimeStopwatch() {
+		return Stopwatch.createStarted(TICKER_THREAD_CPU_TIME);
+	}
+
+	public static void check(final @Nullable Stopwatch stopwatch, final long threshold_millis, final String what) {
+		if (stopwatch == null) return;
+		final long duration = stopwatch.elapsed(TimeUnit.MILLISECONDS);
+		if (duration > threshold_millis) Log.w(TAG, duration + " ms spent in " + what);
+	}
+
+	private static final String TAG = "Performance";
+}
