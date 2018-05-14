2c7ea17caa5d9eb07ea36c0b4c0e7092c61108f6
oasisfeng <git@id.oasisfeng.com>
2018-05-14 23:41:48 +0800

    UPDATE: Minor tweaks to GreenifyClient with support for non-activity caller identity verification.

--- app/src/main/java/com/oasisfeng/island/greenify/GreenifyClient.java
+++ app/src/main/java/com/oasisfeng/island/greenify/GreenifyClient.java
@@ -1,6 +1,7 @@
 package com.oasisfeng.island.greenify;
 
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
@@ -11,9 +12,10 @@ import android.os.UserHandle;
 import android.os.UserManager;
 import android.support.annotation.Nullable;
 
-import com.oasisfeng.android.util.Apps;
-import com.oasisfeng.island.mobile.BuildConfig;
+import com.google.common.base.Preconditions;
+import com.oasisfeng.android.app.Activities;
 
+import static android.app.PendingIntent.FLAG_UPDATE_CURRENT;
 import static android.content.Context.USER_SERVICE;
 
 /**
@@ -23,50 +25,57 @@ import static android.content.Context.USER_SERVICE;
  */
 public class GreenifyClient {
 
-	private static final String GREENIFY_ACTION = "com.oasisfeng.greenify.action.GREENIFY";
-	private static final int MIN_GREENIFY_VERSION = 306;	// Greenify 3.0 (build 5)
+	private static final String ACTION_GREENIFY = "com.oasisfeng.greenify.action.GREENIFY";
+	private static final String EXTRA_CALLER_ID = "caller";
+	private static final int MIN_GREENIFY_VERSION = 306;    // Greenify 3.0 (build 5)
 
-	public static boolean greenify(final Activity activity, final String pkg, final UserHandle user) {
-		final long user_sn = ((UserManager) activity.getSystemService(USER_SERVICE)).getSerialNumberForUser(user);
-		if (user_sn == -1) return false;
-		final Intent intent = new Intent(GREENIFY_ACTION).setPackage(getGreenifyPackage(activity)).setData(Uri.parse("package:" + pkg + "#usn=" + user_sn));
+	public static boolean greenify(final Context context, final String pkg, final UserHandle user) {
+		final long user_sn = Preconditions.checkNotNull((UserManager) context.getSystemService(USER_SERVICE)).getSerialNumberForUser(user);
+		if (user_sn == - 1) return false;
+		final Intent intent = new Intent(ACTION_GREENIFY).setPackage(GREENIFY_PKG).setData(Uri.parse("package:" + pkg + "#usn=" + user_sn));
+		if (getGreenifyVersion(context) > 39500)        // EXTRA_CALLER_ID is supported in newer version of Greenify
+			intent.putExtra(EXTRA_CALLER_ID, PendingIntent.getBroadcast(context, 0, new Intent(), FLAG_UPDATE_CURRENT))
+					.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		final Activity activity = Activities.findActivityFrom(context);
 		try {
-			activity.startActivityForResult(intent, 0);
+			if (activity != null) ((Activity) context).startActivityForResult(intent, 0);
+			else context.startActivity(intent.putExtra("caller", PendingIntent.getBroadcast(context, 0, new Intent(), FLAG_UPDATE_CURRENT)));
 		} catch (final ActivityNotFoundException e) {
 			return false;
 		}
 		return true;
 	}
 
+	private static int getGreenifyVersion(final Context activity) {
+		try {
+			return activity.getPackageManager().getPackageInfo(GREENIFY_PKG, 0).versionCode;
+		} catch (final PackageManager.NameNotFoundException e) {
+			return 0;
+		}
+	}
+
 	public static @Nullable Boolean checkGreenifyVersion(final Context context) {
 		try { @SuppressWarnings("deprecation")
-			final PackageInfo greenify_info = context.getPackageManager().getPackageInfo(getGreenifyPackage(context), PackageManager.GET_UNINSTALLED_PACKAGES);
+			final PackageInfo greenify_info = context.getPackageManager().getPackageInfo(GREENIFY_PKG, PackageManager.GET_UNINSTALLED_PACKAGES);
 			return greenify_info.versionCode >= MIN_GREENIFY_VERSION;
 		} catch (final PackageManager.NameNotFoundException e) {
 			return null;
 		}
 	}
 
-	public static void openInAppMarket(final Activity activity) {
+	public static void openInAppMarket(final Context context) {
+		final Activity activity = Activities.findActivityFrom(context);
+		final Context starter = activity != null ? activity : context;
+		final Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + GREENIFY_PKG)).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 		try {
-			activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + GREENIFY_PKG)));
+			starter.startActivity(intent);
 		} catch (final ActivityNotFoundException e) {
 			try {
-				activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + GREENIFY_PKG)));
+				starter.startActivity(intent.setData(Uri.parse("https://play.google.com/store/apps/details?id=" + GREENIFY_PKG)));
 			} catch (final ActivityNotFoundException ignored) {}
 		}
 	}
 
-	public static String getGreenifyPackage(final @Nullable Context context) {
-		if (context == null) return sGreenifyPackageName != null ? sGreenifyPackageName : GREENIFY_PKG;
-		if (sGreenifyPackageName == null) {
-			if (BuildConfig.DEBUG && Apps.of(context).isInstalledInCurrentUser(GREENIFY_DEBUG_PKG)) sGreenifyPackageName = GREENIFY_DEBUG_PKG;
-			else sGreenifyPackageName = GREENIFY_PKG;
-		}
-		return sGreenifyPackageName;
-	}
-
-	private static String sGreenifyPackageName;
-	private static final String GREENIFY_PKG = "com.oasisfeng.greenify";
-	private static final String GREENIFY_DEBUG_PKG = "com.oasisfeng.greenify.debug";
+	public static final String GREENIFY_PKG = "com.oasisfeng.greenify";
 }
+
