9bef8f3461c230b542261fe5c7f127ca9d8bf361
oasisfeng <git@id.oasisfeng.com>
2018-06-11 21:42:41 +0800

    UPDATE: Minor tweaks.

--- shared/src/main/java/com/oasisfeng/island/notification/NotificationIds.java
+++ shared/src/main/java/com/oasisfeng/island/notification/NotificationIds.java
@@ -9,14 +9,19 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.support.annotation.RequiresPermission;
 import android.support.annotation.StringRes;
 import android.support.v4.app.NotificationManagerCompat;
 
 import com.oasisfeng.island.shared.BuildConfig;
 import com.oasisfeng.island.shared.R;
 
+import java.util.Objects;
+
 import java9.util.function.Consumer;
 
+import static android.app.NotificationManager.IMPORTANCE_HIGH;
+import static android.app.NotificationManager.IMPORTANCE_MIN;
 import static android.os.Build.VERSION.SDK_INT;
 import static android.os.Build.VERSION_CODES.O;
 
@@ -30,7 +35,7 @@ public enum NotificationIds {
 	ShuttleKeeper(Channel.ReadyState),
 	Provisioning(Channel.OngoingTask),
 	UninstallHelper(Channel.Important),
-	Debug(Channel.Important);
+	Debug(Channel.Debug);
 
 	public void post(final Context context, final Notification.Builder notification) {
 		NotificationManagerCompat.from(context).notify(id(), buildChannel(context, notification).build());
@@ -44,7 +49,11 @@ public enum NotificationIds {
 		NotificationManagerCompat.from(context).cancel(id());
 	}
 
-	public void startForeground(final Service service, final Notification.Builder notification) {
+	public void cancel(final Context context, final String tag) {
+		NotificationManagerCompat.from(context).cancel(tag, id());
+	}
+
+	@RequiresPermission("android.permission.FOREGROUND_SERVICE") public void startForeground(final Service service, final Notification.Builder notification) {
 		service.startForeground(id(), buildChannel(service, notification).build());
 	}
 
@@ -61,9 +70,10 @@ public enum NotificationIds {
 
 	@SuppressLint("InlinedApi") enum Channel {
 
-		ReadyState	("ReadyState",	R.string.notification_channel_background_state,	NotificationManager.IMPORTANCE_MIN, channel -> channel.setShowBadge(false)),
-		OngoingTask	("OngoingTask",	R.string.notification_channel_ongoing_task,		NotificationManager.IMPORTANCE_HIGH, channel -> channel.setShowBadge(false)),
-		Important	("Important",	R.string.notification_channel_important,		NotificationManager.IMPORTANCE_HIGH, channel -> channel.setShowBadge(true));
+		ReadyState	("ReadyState",	R.string.notification_channel_background_state,	IMPORTANCE_MIN,  channel -> channel.setShowBadge(false)),
+		OngoingTask	("OngoingTask",	R.string.notification_channel_ongoing_task,		IMPORTANCE_HIGH, channel -> channel.setShowBadge(false)),
+		Important	("Important",	R.string.notification_channel_important,		IMPORTANCE_HIGH, channel -> channel.setShowBadge(true)),
+		Debug		("Debug",		R.string.notification_channel_debug,			IMPORTANCE_MIN,  channel -> channel.setShowBadge(false));
 
 		Channel(final String name, final @StringRes int title, final int importance, final @Nullable Consumer<NotificationChannel> tweaks) {
 			this.name = name; this.title = title; this.importance = importance; this.tweaks = tweaks;
@@ -76,7 +86,7 @@ public enum NotificationIds {
 					throw new Resources.NotFoundException("Placeholder text resource not overridden: " + title);
 				final NotificationChannel channel = new NotificationChannel(name, title_text, importance);
 				if (tweaks != null) tweaks.accept(channel);
-				context.getSystemService(NotificationManager.class).createNotificationChannel(channel);
+				Objects.requireNonNull(context.getSystemService(NotificationManager.class)).createNotificationChannel(channel);
 				created = true;
 			}
 			return name;
