577bebfb3c5b6bf6509291ed497332d149cd3b41
oasisfeng <git@id.oasisfeng.com>
2018-09-17 23:08:47 +0800

    FIX: UninstallHelper now correctly handles BOOT_COMPLETED and is able to restore Island in owner user on Android O+.

--- app/src/main/java/com/oasisfeng/island/UninstallHelper.java
+++ app/src/main/java/com/oasisfeng/island/UninstallHelper.java
@@ -14,6 +14,7 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Environment;
+import android.os.PatternMatcher;
 import android.os.SystemClock;
 import android.os.UserHandle;
 import android.system.ErrnoException;
@@ -59,39 +60,50 @@ public class UninstallHelper extends PseudoContentProvider {
 	private static final String ACTION_RECHECK_ISLAND = "RECHECK_ISLAND";
 
 	@Override public boolean onCreate() {
-		if (! Users.isOwner()) Loopers.addIdleTask(this::check);		// Do nothing in owner user.
+		if (! Users.isOwner()) Loopers.addIdleTask(this::checkAndMonitor);		// Do nothing in owner user.
 		return false;
 	}
 
-	private void check() {
-		final File app_mainland_data_path = new File(new File(Environment.getDataDirectory(), "data"), context().getPackageName());
+	private void checkAndMonitor() {
+		check(context());
+
+		if (SDK_INT >= O) return;		// No longer works on Android O+
+		final LauncherApps launcher = (LauncherApps) context().getSystemService(Context.LAUNCHER_APPS_SERVICE);
+		if (launcher != null) launcher.registerCallback(new LauncherApps.Callback() {
+			@Override public void onPackageRemoved(final String pkg, final UserHandle user) {
+				if (context().getPackageName().equals(pkg) && Users.isOwner(user)) onIslandRemovedInOwnerUser(context());
+			}
+			@Override public void onPackageAdded(final String packageName, final UserHandle user) {}
+			@Override public void onPackageChanged(final String packageName, final UserHandle user) {}
+			@Override public void onPackagesAvailable(final String[] packageNames, final UserHandle user, final boolean replacing) {}
+			@Override public void onPackagesUnavailable(final String[] packageNames, final UserHandle user, final boolean replacing) {}
+		});
+	}
+
+	private static void check(final Context context) {
+		final PackageManager owner_pm;
+		if (Permissions.has(context, INTERACT_ACROSS_USERS) && (owner_pm = ContextShuttle.getPackageManagerAsUser(context, Users.owner)) != null) try { @SuppressLint("WrongConstant")
+			final ApplicationInfo owner_island = owner_pm.getApplicationInfo(context.getPackageName(), Hacks.MATCH_ANY_USER_AND_UNINSTALLED);
+			if ((owner_island.flags & ApplicationInfo.FLAG_INSTALLED) == 0)
+				onIslandRemovedInOwnerUser(context);
+			return;
+		} catch (final PackageManager.NameNotFoundException e) {
+			Log.e(TAG, "Island not found");	// Should not happen
+		}
+
+		final File app_mainland_data_path = new File(new File(Environment.getDataDirectory(), "data"), context.getPackageName());
 		try {
 			final StructStat stat = Os.stat(app_mainland_data_path.getAbsolutePath());
-			if (stat != null) context().getPackageManager().setComponentEnabledSetting(new ComponentName(context(), getClass()),
+			if (stat != null) context.getPackageManager().setComponentEnabledSetting(new ComponentName(context, UninstallHelper.class),
 					COMPONENT_ENABLED_STATE_ENABLED, DONT_KILL_APP);	// Mark as enabled if path is compatible.
 		} catch (final ErrnoException e) {
 			if (e.errno != OsConstants.ENOENT) {
 				Analytics.$().report(e);
 				return;
 			}
-			if (COMPONENT_ENABLED_STATE_ENABLED == context().getPackageManager().getComponentEnabledSetting(new ComponentName(context(), getClass()))) {
-				onIslandRemovedInOwnerUser(context());		// Only if the path compatibility is confirmed when it was still installed.
-				return;
+			if (COMPONENT_ENABLED_STATE_ENABLED == context.getPackageManager().getComponentEnabledSetting(new ComponentName(context, UninstallHelper.class))) {
+				onIslandRemovedInOwnerUser(context);		// Only if the path compatibility is confirmed when it was still installed.
 			} else Log.w(TAG, "Uninstallation state in mainland cannot be detected due to incompatibility");
-		}
-
-		if (SDK_INT < O) {		// No longer works on Android O+
-			final LauncherApps launcher = (LauncherApps) context().getSystemService(Context.LAUNCHER_APPS_SERVICE);
-			if (launcher != null) launcher.registerCallback(new LauncherApps.Callback() {
-				@Override public void onPackageRemoved(final String pkg, final UserHandle user) {
-					if (context().getPackageName().equals(pkg) && Users.isOwner(user)) onIslandRemovedInOwnerUser(context());
-				}
-
-				@Override public void onPackageAdded(final String packageName, final UserHandle user) {}
-				@Override public void onPackageChanged(final String packageName, final UserHandle user) {}
-				@Override public void onPackagesAvailable(final String[] packageNames, final UserHandle user, final boolean replacing) {}
-				@Override public void onPackagesUnavailable(final String[] packageNames, final UserHandle user, final boolean replacing) {}
-			});
 		}
 	}
 
@@ -99,17 +111,32 @@ public class UninstallHelper extends PseudoContentProvider {
 		final Notification.Action action_destroy = new Notification.Action.Builder(R.drawable.ic_delete_forever_white_24dp, context.getString(R.string.dialog_button_destroy),
 				getBroadcast(context, 0, new Intent(context, UninstallReceiver.class).setAction(ACTION_DESTROY_ISLAND), FLAG_UPDATE_CURRENT)).build();
 		final Notification.Builder n = new Notification.Builder(context).setOngoing(true).setSmallIcon(android.R.drawable.stat_sys_warning)
-				.setContentTitle(context.getString(R.string.notification_remove_island_after_uninstall_title))
-				.setContentText(context.getString(R.string.notification_remove_island_after_uninstall_text))
-				.addAction(action_destroy);
-		// There's a bug in Android O which prevents Package Installer from correctly identifying the caller package (per-app package install permission)
-		// not installed in the same user as Package Installer. That unfortunately is our case.
-		if (SDK_INT < O) {
-			final Notification.Action action_restore = new Notification.Action.Builder(R.drawable.ic_undo_white_24dp, context.getString(R.string.notification_action_restore),
-					getBroadcast(context, 0, new Intent(context, UninstallReceiver.class).setAction(ACTION_RESTORE_ISLAND), FLAG_UPDATE_CURRENT)).build();
-			n.addAction(action_restore);
-		}
+				.setContentTitle(context.getString(R.string.notification_island_after_uninstall_title))
+				.setContentText(context.getString(R.string.notification_island_after_uninstall_text))
+				.addAction(action_destroy).addAction(new Notification.Action.Builder(R.drawable.ic_undo_white_24dp, context.getString(R.string.notification_action_restore),
+				getBroadcast(context, 0, new Intent(context, UninstallReceiver.class).setAction(ACTION_RESTORE_ISLAND), FLAG_UPDATE_CURRENT)).build());
 		NotificationIds.UninstallHelper.post(context, n);
+	}
+
+	private static void openIslandWithinAppMarketInOwnerUser(final Context context) {
+		startActivityByCrossProfileIntentFilter(context, new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + context.getPackageName())));
+	}
+
+	private static void reinstallInOwnerUserWithPackageInstaller(final Context context) {
+		startActivityByCrossProfileIntentFilter(context, new Intent(Intent.ACTION_INSTALL_PACKAGE, Uri.parse("package:" + context.getPackageName())));
+	}
+
+	private static boolean startActivityByCrossProfileIntentFilter(final Context context, final Intent intent) {
+		final Uri uri = Objects.requireNonNull(intent.getData());
+		new DevicePolicies(context).addCrossProfileIntentFilter(IntentFilters.forAction(intent.getAction())
+				.withData(uri.getScheme(), uri.getSchemeSpecificPart(), PatternMatcher.PATTERN_LITERAL), FLAG_PARENT_CAN_ACCESS_MANAGED);
+		@SuppressLint("WrongConstant") final List<ResolveInfo> resolves
+				= context.getPackageManager().queryIntentActivities(intent, Hacks.MATCH_ANY_USER_AND_UNINSTALLED);
+		for (final ResolveInfo resolve : resolves) if ("android".equals(resolve.activityInfo.packageName)) {		// IntentForwarder
+			context.startActivity(intent.setComponent(new ComponentName(resolve.activityInfo.packageName, resolve.activityInfo.name)));
+			return true;
+		}
+		return false;
 	}
 
 	public static class UninstallReceiver extends BroadcastReceiver {
@@ -118,16 +145,7 @@ public class UninstallHelper extends PseudoContentProvider {
 			if (Users.isOwner()) return;
 			final String action = intent.getAction();
 			if (ACTION_RECHECK_ISLAND.equals(action) || Intent.ACTION_BOOT_COMPLETED.equals(action)) {
-				if (! Permissions.has(context, INTERACT_ACROSS_USERS)) return;		// FIXME: Solution if no permission INTERACT_ACROSS_USERS
-				final PackageManager owner_pm = ContextShuttle.getPackageManagerAsUser(context, Users.owner);
-				if (owner_pm == null) return;
-				try { @SuppressLint("WrongConstant")
-					final ApplicationInfo owner_island = owner_pm.getApplicationInfo(context.getPackageName(), Hacks.MATCH_ANY_USER_AND_UNINSTALLED);
-					if ((owner_island.flags & ApplicationInfo.FLAG_INSTALLED) == 0)
-						onIslandRemovedInOwnerUser(context);
-				} catch (final PackageManager.NameNotFoundException e) {
-					Log.e(TAG, "Island not found");
-				}
+				check(context);
 			} else if (ACTION_DESTROY_ISLAND.equals(action)) {
 				final DevicePolicyManager dpm = Objects.requireNonNull((DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE));
 				dpm.wipeData(0);
@@ -139,16 +157,10 @@ public class UninstallHelper extends PseudoContentProvider {
 				final Intent recheck = new Intent(context, getClass()).setAction(ACTION_RECHECK_ISLAND);
 				am.set(AlarmManager.ELAPSED_REALTIME, now + 180_000, getBroadcast(context, 0, recheck, FLAG_UPDATE_CURRENT));
 
-				final Intent install_intent = new Intent(Intent.ACTION_INSTALL_PACKAGE, Uri.fromParts("package", context.getPackageName(), null));
-				new DevicePolicies(context).addCrossProfileIntentFilter
-						(IntentFilters.forAction(install_intent.getAction()).withDataScheme(install_intent.getScheme()), FLAG_PARENT_CAN_ACCESS_MANAGED);
-				@SuppressLint("WrongConstant") final List<ResolveInfo> resolves
-						= context.getPackageManager().queryIntentActivities(install_intent, Hacks.MATCH_ANY_USER_AND_UNINSTALLED);
-				for (final ResolveInfo resolve : resolves)
-					if ("android".equals(resolve.activityInfo.packageName)) {		// IntentForwarder
-						context.startActivity(install_intent.setComponent(new ComponentName(resolve.activityInfo.packageName, resolve.activityInfo.name)));
-						return;
-					}
+				// There's a bug in Android O which prevents Package Installer from correctly identifying the target SDK version of calling package
+				//   not installed in the same user as Package Installer. Unfortunately that is our case.
+				if (SDK_INT >= O) openIslandWithinAppMarketInOwnerUser(context);
+				else reinstallInOwnerUserWithPackageInstaller(context);
 			}
 		}
 	}
