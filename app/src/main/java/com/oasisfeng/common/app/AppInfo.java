oasisfeng/island

commit 3d99bee5e8b13b54dec4faf061647c99921ba4a2

FIX: MATCH_ANY_USER is still not correctly used in ApiDispatcher, causing crash.

engine/src/main/java/com/oasisfeng/island/api/ApiDispatcher.java
@@ -17,7 +17,6 @@ import android.util.ArrayMap;
 import android.util.Log;
 
 import com.oasisfeng.island.engine.IslandManager;
-import com.oasisfeng.island.util.Builds;
 import com.oasisfeng.island.util.Hacks;
 import com.oasisfeng.island.util.Permissions;
 import com.oasisfeng.island.util.Users;
@@ -34,8 +33,10 @@ import java9.util.stream.Stream;
 import java9.util.stream.StreamSupport;
 
 import static android.content.pm.PackageManager.GET_SIGNATURES;
+import static android.content.pm.PackageManager.GET_UNINSTALLED_PACKAGES;
 import static android.os.Build.VERSION.SDK_INT;
 import static android.os.Build.VERSION_CODES.M;
+import static android.os.Build.VERSION_CODES.P;
 import static com.oasisfeng.android.Manifest.permission.INTERACT_ACROSS_USERS;
 
 /**
@@ -77,12 +78,12 @@ class ApiDispatcher {
 		final int signature_hash = value;
 		if (signature_hash == 0) return null;	// 0 means already verified (cached)
 
-		if (Builds.isAndroidPIncludingPreviews() && ! Users.isOwner() && ! Permissions.has(context, INTERACT_ACROSS_USERS))
-			return "Permission denied";		// Legacy verification is not supported on Android P+, due to MATCH_ANY_USER being restricted.
+		// Legacy verification is not supported inside Island without INTERACT_ACROSS_USERS on Android P+, due to MATCH_ANY_USER being restricted.
 		try { @SuppressWarnings("deprecation") @SuppressLint({"PackageManagerGetSignatures", "WrongConstant"})
-			final PackageInfo pkg_info = context.getPackageManager().getPackageInfo(pkg, GET_SIGNATURES | Hacks.MATCH_ANY_USER_AND_UNINSTALLED);
+			final PackageInfo pkg_info = context.getPackageManager().getPackageInfo(pkg, GET_SIGNATURES
+				| (SDK_INT < P || Permissions.has(context, INTERACT_ACROSS_USERS) ? Hacks.MATCH_ANY_USER_AND_UNINSTALLED : GET_UNINSTALLED_PACKAGES));
 			return verifySignature(pkg, signature_hash, pkg_info);
-		} catch (final PackageManager.NameNotFoundException e) { return "Client package not found: " + pkg; }		// Should hardly happen
+		} catch (final PackageManager.NameNotFoundException e) { return "Permission denied or client package not found: " + pkg; }
 	}
 
 	@Nullable private static String verifySignature(final String pkg, final int signature_hash, final PackageInfo pkg_info) {

shared/src/main/java/com/oasisfeng/island/util/Hacks.java
@@ -47,12 +47,15 @@ public class Hacks {
 	}
 
 	private static final int MATCH_ANY_USER = 0x00400000;		// Requires INTERACT_ACROSS_USERS since Android P.
-	// When used in @ApplicationInfoFlags or @PackageInfoFlags:
-	//   For owner user, GET_UNINSTALLED_PACKAGES implicitly set MATCH_ANY_USER.
-	//   For managed profile, MATCH_ANY_USER requires permission INTERACT_ACROSS_USERS.
-	// Otherwise no permission required.
-	// See PackageManagerService.updateFlagsForPackage()
-	public static final int MATCH_ANY_USER_AND_UNINSTALLED = PackageManager.GET_UNINSTALLED_PACKAGES | MATCH_ANY_USER;
+	/**
+	 * When used in @ApplicationInfoFlags or @PackageInfoFlags:
+	 *   For owner user, GET_UNINSTALLED_PACKAGES implicitly set MATCH_ANY_USER.
+	 *   For managed profile, MATCH_ANY_USER requires permission INTERACT_ACROSS_USERS since Android P.
+	 * When used in @ComponentInfoFlags or @ResolveInfoFlags: MATCH_ANY_USER is always allowed.
+	 *
+	 * See PackageManagerService.updateFlagsForPackage()
+	 */
+	public static final int MATCH_ANY_USER_AND_UNINSTALLED = PackageManager.GET_UNINSTALLED_PACKAGES | (Users.isOwner() ? 0 : MATCH_ANY_USER);
 
 	public static final Hack.HackedField<ApplicationInfo, Integer>
 			ApplicationInfo_privateFlags = Hack.onlyIf(SDK_INT >= M).into(ApplicationInfo.class).field("privateFlags").fallbackTo(null);

commit c5f45cc8077dc42529e43e3f1d1f3d2967ba7f37

FIX: Island-exclusive app cannot be launched.
Hopefully fixed all MATCH_ANY_USER-related issues, once for all.

This bug was introduced in commit 3d99bee5e8b13b54dec4faf061647c99921ba4a2.

app/src/main/java/com/oasisfeng/common/app/AppInfo.java
@@ -48,9 +48,9 @@ public class AppInfo extends ApplicationInfo {
 	public boolean isLaunchable() { return mIsLaunchable.get(); }
 	private final Supplier<Boolean> mIsLaunchable = lazyLessMutable(() -> checkLaunchable(0));
 
-	protected boolean checkLaunchable(final int flags) {
+	protected boolean checkLaunchable(final int flags_for_resolve) {
 		final Intent intent = new Intent(Intent.ACTION_MAIN).addCategory(Intent.CATEGORY_LAUNCHER).setPackage(packageName);
-		final ResolveInfo resolved = context().getPackageManager().resolveActivity(intent, flags);
+		final ResolveInfo resolved = context().getPackageManager().resolveActivity(intent, flags_for_resolve);
 		return resolved != null;
 	}
 

app/src/main/java/com/oasisfeng/island/UninstallHelper.java
@@ -83,7 +83,7 @@ public class UninstallHelper extends PseudoContentProvider {
 	private static void check(final Context context) {
 		final PackageManager owner_pm;
 		if (Permissions.has(context, INTERACT_ACROSS_USERS) && (owner_pm = ContextShuttle.getPackageManagerAsUser(context, Users.owner)) != null) try { @SuppressLint("WrongConstant")
-			final ApplicationInfo owner_island = owner_pm.getApplicationInfo(context.getPackageName(), Hacks.MATCH_ANY_USER_AND_UNINSTALLED);
+			final ApplicationInfo owner_island = owner_pm.getApplicationInfo(context.getPackageName(), Hacks.GET_ANY_USER_AND_UNINSTALLED);
 			if ((owner_island.flags & ApplicationInfo.FLAG_INSTALLED) == 0)
 				onIslandRemovedInOwnerUser(context);
 			return;
@@ -130,8 +130,7 @@ public class UninstallHelper extends PseudoContentProvider {
 		final Uri uri = Objects.requireNonNull(intent.getData());
 		new DevicePolicies(context).addCrossProfileIntentFilter(IntentFilters.forAction(intent.getAction())
 				.withData(uri.getScheme(), uri.getSchemeSpecificPart(), PatternMatcher.PATTERN_LITERAL), FLAG_PARENT_CAN_ACCESS_MANAGED);
-		@SuppressLint("WrongConstant") final List<ResolveInfo> resolves
-				= context.getPackageManager().queryIntentActivities(intent, Hacks.MATCH_ANY_USER_AND_UNINSTALLED);
+		final List<ResolveInfo> resolves = context.getPackageManager().queryIntentActivities(intent, 0);
 		for (final ResolveInfo resolve : resolves) if ("android".equals(resolve.activityInfo.packageName)) {		// IntentForwarder
 			context.startActivity(intent.setComponent(new ComponentName(resolve.activityInfo.packageName, resolve.activityInfo.name)));
 			return true;

app/src/main/java/com/oasisfeng/island/data/IslandAppInfo.java
@@ -82,16 +82,16 @@ public class IslandAppInfo extends AppInfo {
 
 	/** Is launchable (even if hidden) */
 	@Override public boolean isLaunchable() { return mIsLaunchable.get(); }
-	private final Supplier<Boolean> mIsLaunchable = Suppliers.memoizeWithExpiration(() -> checkLaunchable(Hacks.MATCH_ANY_USER_AND_UNINSTALLED), 1, SECONDS);
+	private final Supplier<Boolean> mIsLaunchable = Suppliers.memoizeWithExpiration(() -> checkLaunchable(Hacks.RESOLVE_ANY_USER_AND_UNINSTALLED), 1, SECONDS);
 
-	@Override protected boolean checkLaunchable(final int flags) {
+	@Override protected boolean checkLaunchable(final int flags_for_resolve) {
 		if (sLaunchableAppsCache != null) return sLaunchableAppsCache.contains(packageName);
-		return super.checkLaunchable(flags);
+		return super.checkLaunchable(flags_for_resolve);
 	}
 
 	public static void cacheLaunchableApps(final Context context) {
 		@SuppressLint("WrongConstant") final List<ResolveInfo> activities = context.getPackageManager()
-				.queryIntentActivities(new Intent(Intent.ACTION_MAIN).addCategory(Intent.CATEGORY_LAUNCHER), Hacks.MATCH_ANY_USER_AND_UNINSTALLED);
+				.queryIntentActivities(new Intent(Intent.ACTION_MAIN).addCategory(Intent.CATEGORY_LAUNCHER), Hacks.RESOLVE_ANY_USER_AND_UNINSTALLED);
 		sLaunchableAppsCache = StreamSupport.stream(activities).map(resolve -> resolve.activityInfo.packageName).collect(Collectors.toSet());
 	}
 

app/src/main/java/com/oasisfeng/island/data/IslandAppListProvider.java
@@ -285,7 +285,7 @@ public class IslandAppListProvider extends AppListProvider<IslandAppInfo> {
 	private final Supplier<ClonedHiddenSystemApps> mClonedHiddenSystemApps = Suppliers.memoize(() ->
 			new ClonedHiddenSystemApps(context(), Users.profile, pkg -> refreshPackage(pkg, Users.profile, false)));
 	private final Supplier<Set<String>> mCriticalSystemPackages = Suppliers.memoize(() ->
-			SystemAppsManager.detectCriticalSystemPackages(context().getPackageManager(), Hacks.MATCH_ANY_USER_AND_UNINSTALLED));
+			SystemAppsManager.detectCriticalSystemPackages(context().getPackageManager()));
 
 	private static final String TAG = "Island.AppListProv";
 }

engine/src/main/java/com/oasisfeng/island/api/ApiActivity.java
@@ -25,7 +25,7 @@ public class ApiActivity extends Activity {
 			String result;
 			final ComponentName caller = getCallingActivity();
 			if (caller != null) try { @SuppressLint("WrongConstant")		// Invoked with startActivityForResult()
-				final ActivityInfo info = getPackageManager().getActivityInfo(caller, Hacks.MATCH_ANY_USER_AND_UNINSTALLED);
+				final ActivityInfo info = getPackageManager().getActivityInfo(caller, Hacks.RESOLVE_ANY_USER_AND_UNINSTALLED);
 				int uid = info.applicationInfo.uid;
 				if (UserHandles.getUserId(uid) != 0) {
 					final String[] potential_pkgs = getPackageManager().getPackagesForUid(uid);

engine/src/main/java/com/oasisfeng/island/api/ApiDispatcher.java
@@ -81,7 +81,7 @@ class ApiDispatcher {
 		// Legacy verification is not supported inside Island without INTERACT_ACROSS_USERS on Android P+, due to MATCH_ANY_USER being restricted.
 		try { @SuppressWarnings("deprecation") @SuppressLint({"PackageManagerGetSignatures", "WrongConstant"})
 			final PackageInfo pkg_info = context.getPackageManager().getPackageInfo(pkg, GET_SIGNATURES
-				| (SDK_INT < P || Permissions.has(context, INTERACT_ACROSS_USERS) ? Hacks.MATCH_ANY_USER_AND_UNINSTALLED : GET_UNINSTALLED_PACKAGES));
+				| (SDK_INT < P || Permissions.has(context, INTERACT_ACROSS_USERS) ? Hacks.GET_ANY_USER_AND_UNINSTALLED : GET_UNINSTALLED_PACKAGES));
 			return verifySignature(pkg, signature_hash, pkg_info);
 		} catch (final PackageManager.NameNotFoundException e) { return "Permission denied or client package not found: " + pkg; }
 	}

engine/src/main/java/com/oasisfeng/island/provisioning/IslandProvisioning.java
@@ -33,7 +33,6 @@ import com.oasisfeng.island.notification.NotificationIds;
 import com.oasisfeng.island.shortcut.AbstractAppLaunchShortcut;
 import com.oasisfeng.island.shuttle.ServiceShuttle;
 import com.oasisfeng.island.util.DevicePolicies;
-import com.oasisfeng.island.util.Hacks;
 import com.oasisfeng.island.util.Modules;
 import com.oasisfeng.island.util.OwnerUser;
 import com.oasisfeng.island.util.Permissions;
@@ -212,7 +211,7 @@ public class IslandProvisioning extends IntentService {
 	}
 
 	@ProfileUser private static void enableCriticalAppsIfNeeded(final Context context, final DevicePolicies policies) {
-		final Set<String> pkgs = CriticalAppsManager.detectCriticalPackages(context.getPackageManager(), Hacks.MATCH_ANY_USER_AND_UNINSTALLED);
+		final Set<String> pkgs = CriticalAppsManager.detectCriticalPackages(context.getPackageManager());
 		for (final String pkg : pkgs) try {
 			policies.enableSystemApp(pkg);        // FIXME: Don't re-enable explicitly cloned system apps. (see ClonedHiddenSystemApps)
 			policies.invoke(DevicePolicyManager::setApplicationHidden, pkg, false);

shared/src/main/java/com/oasisfeng/island/provisioning/CriticalAppsManager.java
@@ -24,8 +24,8 @@ import static android.os.Build.VERSION_CODES.N;
  */
 public class CriticalAppsManager {
 
-	@ProfileUser static Set<String> detectCriticalPackages(final PackageManager pm, final int flags) {
-		final Set<String> pkgs = SystemAppsManager.detectCriticalSystemPackages(pm, flags);
+	@ProfileUser static Set<String> detectCriticalPackages(final PackageManager pm) {
+		final Set<String> pkgs = SystemAppsManager.detectCriticalSystemPackages(pm);
 
 		if (SDK_INT >= N) {		// Chrome web-view is supported on Android 7+.
 			final String webview_pkg = getCurrentWebViewPackageName();

shared/src/main/java/com/oasisfeng/island/provisioning/SystemAppsManager.java
@@ -129,15 +129,14 @@ import static android.os.Build.VERSION_CODES.N;
 		} catch (final PackageManager.NameNotFoundException ignored) { return null; }
 	}
 
-	/** @param flags intersection of {@code PackageManager.ComponentInfoFlags} and {@code PackageManager.ResolveInfoFlags}. */
-	@OwnerUser @ProfileUser public static Set<String> detectCriticalSystemPackages(final PackageManager pm, final int flags) {
+	@OwnerUser @ProfileUser public static Set<String> detectCriticalSystemPackages(final PackageManager pm) {
 		final Stopwatch stopwatch = Performances.startUptimeStopwatch();
 
 		final Set<String> critical_sys_pkgs = new HashSet<>(sCriticalSystemPkgs);
 
 		// Detect package names for critical intent actions, as an addition to the white-list of well-known ones.
-		for (final Intent intent : sCriticalActivityIntents) {
-			@SuppressLint("WrongConstant") final ResolveInfo info = pm.resolveActivity(intent, MATCH_DEFAULT_ONLY | Hacks.MATCH_ANY_USER_AND_UNINSTALLED);
+		for (final Intent intent : sCriticalActivityIntents) { @SuppressLint("WrongConstant")
+			final ResolveInfo info = pm.resolveActivity(intent, MATCH_DEFAULT_ONLY | Hacks.RESOLVE_ANY_USER_AND_UNINSTALLED);
 			if (info == null || (info.activityInfo.applicationInfo.flags & FLAG_SYSTEM) == 0) continue;
 			final String pkg = info.activityInfo.packageName;
 			Log.i(TAG, "Critical package for " + intent + ": " + pkg);
@@ -148,7 +147,7 @@ import static android.os.Build.VERSION_CODES.N;
 		// Detect package names for critical content providers, as an addition to the white-list of well-known ones.
 		for (final String authority : sCriticalContentAuthorities) {
 			if (authority == null) continue;		// Nullable for version-specific authorities
-			final ProviderInfo provider = pm.resolveContentProvider(authority, flags);
+			@SuppressLint("WrongConstant") final ProviderInfo provider = pm.resolveContentProvider(authority, Hacks.RESOLVE_ANY_USER_AND_UNINSTALLED);
 			if (provider == null || (provider.applicationInfo.flags & FLAG_SYSTEM) == 0 || (provider.flags & FLAG_SINGLE_USER) != 0) continue;
 			Log.i(TAG, "Critical package for authority \"" + authority + "\": " + provider.packageName);
 			critical_sys_pkgs.add(provider.packageName);

shared/src/main/java/com/oasisfeng/island/util/Hacks.java
@@ -55,7 +55,8 @@ public class Hacks {
 	 *
 	 * See PackageManagerService.updateFlagsForPackage()
 	 */
-	public static final int MATCH_ANY_USER_AND_UNINSTALLED = PackageManager.GET_UNINSTALLED_PACKAGES | (Users.isOwner() ? 0 : MATCH_ANY_USER);
+	public static final int GET_ANY_USER_AND_UNINSTALLED = PackageManager.GET_UNINSTALLED_PACKAGES | (Users.isOwner() ? 0 : MATCH_ANY_USER);
+	public static final int RESOLVE_ANY_USER_AND_UNINSTALLED = PackageManager.GET_UNINSTALLED_PACKAGES | MATCH_ANY_USER;
 
 	public static final Hack.HackedField<ApplicationInfo, Integer>
 			ApplicationInfo_privateFlags = Hack.onlyIf(SDK_INT >= M).into(ApplicationInfo.class).field("privateFlags").fallbackTo(null);

shared/src/main/java/com/oasisfeng/island/util/Modules.java
@@ -61,7 +61,7 @@ public class Modules {
 
 	private static ComponentName resolveActivity(final Context context, final Intent intent) {
 		@SuppressLint("WrongConstant") final ResolveInfo resolved = context.getPackageManager().resolveActivity(intent,
-				MATCH_DEFAULT_ONLY | GET_DISABLED_COMPONENTS | Hacks.MATCH_ANY_USER_AND_UNINSTALLED);
+				MATCH_DEFAULT_ONLY | GET_DISABLED_COMPONENTS | Hacks.RESOLVE_ANY_USER_AND_UNINSTALLED);
 		return resolved == null ? null : new ComponentName(resolved.activityInfo.packageName, resolved.activityInfo.name);
 	}
 