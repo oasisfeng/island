4030d47fc100ad6aaf334e84bc72cbde595c2e3d
oasisfeng <git@id.oasisfeng.com>
2019-05-07 10:41:50 +0800

    UPDATE: Support cross-profile binding in open API container.

--- open/src/main/aidl/com/oasisfeng/island/IContainer.aidl
+++ open/src/main/aidl/com/oasisfeng/island/IContainer.aidl
@@ -1,9 +1,10 @@
 package com.oasisfeng.island;
 
 import android.content.ComponentName;
+import android.content.pm.ApplicationInfo;
 
 /** The internal AIDL interface for controlling isolated container */
 interface IContainer {
     void registerSystemService(String name, in IBinder service);
-    IBinder loadService(in ComponentName service);
+    IBinder loadService(in ComponentName service, in ApplicationInfo info);
 }
--- open/src/main/java/com/oasisfeng/island/ContainerService.java
+++ open/src/main/java/com/oasisfeng/island/ContainerService.java
@@ -1,23 +1,28 @@
 package com.oasisfeng.island;
 
 import android.annotation.SuppressLint;
+import android.app.AppOpsManager;
 import android.app.Application;
 import android.app.Service;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInstaller;
 import android.content.pm.PackageManager;
 import android.os.IBinder;
+import android.os.Process;
 import android.os.RemoteException;
+import android.os.UserHandle;
 import android.util.Log;
 
 import com.oasisfeng.island.api.DelegatedAppOpsManager;
 import com.oasisfeng.island.api.DelegatedDevicePolicyManager;
 import com.oasisfeng.island.api.DelegatedPackageManager;
+import com.oasisfeng.island.shuttle.MethodShuttle;
 import com.oasisfeng.island.util.DeviceAdmins;
-import com.oasisfeng.island.util.Hacks;
+import com.oasisfeng.island.util.Users;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -25,6 +30,7 @@ import java.lang.reflect.Method;
 import androidx.annotation.Nullable;
 
 import static android.app.admin.DevicePolicyManager.EXTRA_DEVICE_ADMIN;
+import static android.content.pm.PackageManager.GET_UNINSTALLED_PACKAGES;
 
 /**
  * Isolated container service to run unsafe 3rd-party code.
@@ -36,48 +42,87 @@ public class ContainerService extends Service {
 	public static class Launcher extends Service {
 
 		@Nullable @Override public IBinder onBind(final Intent intent) {
-			Log.d(TAG, "Requested with " + intent);
 			final Intent selector = intent.getSelector();
 			final ComponentName service_component = selector != null ? selector.getComponent() : null;
-			if (service_component != null) try {
-				return launch(this, service_component);
-			} catch (final ReflectiveOperationException e) {
-				Log.e(TAG, "Error launching container", e);
-			} else Log.w(TAG, "Target service component must be provided as selector intent via Intent.setSelector().");
+			if (service_component == null) {
+				Log.w(TAG, "Target service component must be provided as selector intent via Intent.setSelector().");
+				return null;
+			}
+
+			final UserHandle specified_user = intent.getParcelableExtra(Intent.EXTRA_USER);
+			if (specified_user != null && ! Users.isProfile(specified_user)) {
+				Log.w(TAG, specified_user + " is not a valid managed profile to bind service inside.");
+				return null;
+			}
+			final UserHandle user = specified_user == null || Process.myUserHandle().equals(specified_user) ? null : specified_user;
+
+			Log.d(TAG, "Requested binding to " + service_component.flattenToShortString() + (user != null ? " in " + user : ""));
+			final LatchedBinderProxy latched_binder_proxy = new LatchedBinderProxy();
+			if (user != null) {
+				final ApplicationInfo app_info;
+				try {
+					app_info = getPackageManager().getApplicationInfo(service_component.getPackageName(), GET_UNINSTALLED_PACKAGES);
+				} catch (final PackageManager.NameNotFoundException e) {
+					Log.e(TAG, "Package of target service is not found: " + service_component.getPackageName());
+					return null;
+				}
+				final Context context = this;
+				MethodShuttle.runInProfile(this, () -> launch(context, service_component, latched_binder_proxy, app_info))
+						.thenAccept(result -> { if (! result) latched_binder_proxy.cancel(null); })
+						.exceptionally(t -> { latched_binder_proxy.cancel(t instanceof Exception ? (Exception) t : new Exception(t)); return null; });
+				return latched_binder_proxy;
+			} else {
+				final boolean result = launch(this, service_component, latched_binder_proxy, null);
+				if (result) return latched_binder_proxy;
+				Log.e(TAG, "Failed to bind to container.");
+			}
 			return null;
 		}
 
-		private IBinder launch(final Context context, final ComponentName service) throws ReflectiveOperationException {
-			final DelegatedDevicePolicyManager delegated_dpm = new DelegatedDevicePolicyManager(context);
-			final RestrictedBinderProxy delegated_dpm_proxy = delegated_dpm.getDelegatedBinderProxy();
-			delegated_dpm.getCameraDisabled(null);		// Allowed API. TODO: Test
-			delegated_dpm_proxy.seal();
-
-			final DelegatedAppOpsManager delegated_aom = new DelegatedAppOpsManager(context);
-			final RestrictedBinderProxy delegated_aom_proxy = delegated_aom.getDelegatedBinderProxy();
-			Hacks.AppOpsManager_setMode.invoke(0, 0, null, 0).on(delegated_aom);		// Allowed API. TODO: Test
-			delegated_aom_proxy.seal();
-
-			final IBinder package_installer_binder = DelegatedPackageManager.getPackageInstallerBinder(context.getPackageManager().getPackageInstaller());
-
-			final LatchedBinderProxy latched_binder_proxy = new LatchedBinderProxy();
+		private static boolean launch(final Context context, final ComponentName service, final LatchedBinderProxy latched_binder_proxy, final @Nullable ApplicationInfo app_info) {
 			final Intent intent = new Intent(context, ContainerService.class).putExtra(EXTRA_DEVICE_ADMIN, DeviceAdmins.getComponentName(context));
-			final boolean result = context.bindService(intent, mServiceConnection = new ServiceConnection() {
+			final ServiceConnection connection = new ServiceConnection() {
 
 				@Override public void onServiceConnected(final ComponentName name, final IBinder binder) {
-					mServiceConnection = this;
-					Log.i(TAG, "Preparing container");
+					if (context instanceof Launcher) ((Launcher) context).mServiceConnection = this;
+					else context.unbindService(this);		// Just one-time binding for cross-profile launch, due to ServiceConnection incapable across profile.
+					Log.i(TAG, "Preparing container");		// Delegated services must be created within the same user of requested service.
+					RestrictedBinderProxy delegated_dpm_proxy = null;
+					try {
+						final DelegatedDevicePolicyManager delegated_dpm = new DelegatedDevicePolicyManager(context);
+						delegated_dpm_proxy = delegated_dpm.getDelegatedBinderProxy();
+						// Allowed API. TODO: Test
+						delegated_dpm.setApplicationHidden(null, null, false);
+						delegated_dpm.isApplicationHidden(null, null);
+						delegated_dpm_proxy.seal();
+					} catch (final ReflectiveOperationException e) {
+						Log.e(TAG, "Error preparing delegated DevicePolicyManager", e);
+					}
+
+					RestrictedBinderProxy delegated_aom_proxy = null;
+					try {
+						final DelegatedAppOpsManager delegated_aom = new DelegatedAppOpsManager(context);
+						delegated_aom_proxy = delegated_aom.getDelegatedBinderProxy();
+						// Allowed API. TODO: Test
+						AppOpsManager.class.getMethod("setMode", int.class, int.class, String.class, int.class).invoke(delegated_aom, 0, 0, null, 0);
+						delegated_aom_proxy.seal();
+					} catch (final ReflectiveOperationException e) {
+						Log.e(TAG, "Error preparing delegated AppOpsManager", e);
+					}
+
+					final IBinder package_installer_binder = DelegatedPackageManager.getPackageInstallerBinder(context.getPackageManager().getPackageInstaller());
+
 					final IContainer container = IContainer.Stub.asInterface(binder);
 					try {
-						container.registerSystemService(Context.DEVICE_POLICY_SERVICE, delegated_dpm_proxy);
-						container.registerSystemService(Context.APP_OPS_SERVICE, delegated_aom_proxy);
+						if (delegated_dpm_proxy != null) container.registerSystemService(Context.DEVICE_POLICY_SERVICE, delegated_dpm_proxy);
+						if (delegated_aom_proxy != null) container.registerSystemService(Context.APP_OPS_SERVICE, delegated_aom_proxy);
 						container.registerSystemService(PackageInstaller.class.getName(), package_installer_binder);
 					} catch (final RemoteException e) {
 						latched_binder_proxy.cancel(new IllegalStateException("Error preparing container", e));
 					}
 					Log.i(TAG, "Binding to requested service: " + service.flattenToShortString());
 					try {
-						final IBinder external_service = container.loadService(service);
+						final IBinder external_service = container.loadService(service, app_info);
 						if (external_service != null) latched_binder_proxy.setDelegate(external_service);
 						else latched_binder_proxy.cancel(null);
 					} catch (final Exception e) {
@@ -85,12 +130,9 @@ public class ContainerService extends Service {
 					}
 				}
 
-				@Override public void onServiceDisconnected(final ComponentName name) {}	// TODO: The external service crashed, how to handle?
-			}, Context.BIND_AUTO_CREATE | Context.BIND_IMPORTANT);
-
-			if (result) return latched_binder_proxy;
-			Log.e(TAG, "Failed to bind to container.");
-			return null;
+				@Override public void onServiceDisconnected(final ComponentName name) {}    // TODO: The external service crashed, how to handle?
+			};
+			return context.bindService(intent, connection, Context.BIND_AUTO_CREATE | Context.BIND_IMPORTANT);
 		}
 
 		@Override public void onDestroy() {
@@ -129,11 +171,15 @@ public class ContainerService extends Service {
 			}
 		}
 
-		@SuppressLint("PrivateApi") @Override public IBinder loadService(final ComponentName component) throws RemoteException {
+		@SuppressLint("PrivateApi") @Override public IBinder loadService(final ComponentName component, final ApplicationInfo app_info) throws RemoteException {
 			final String flat_component = component.flattenToShortString();
 			final Service service; final Method Service_attach;
 			try {
-				final Context service_context = createPackageContext(component.getPackageName(), CONTEXT_INCLUDE_CODE | CONTEXT_IGNORE_SECURITY);
+				final Context service_context;
+				if (app_info != null) { //noinspection JavaReflectionMemberAccess
+					service_context = (Context) Context.class.getMethod("createApplicationContext", ApplicationInfo.class, int.class)
+							.invoke(ContainerService.this, app_info, CONTEXT_INCLUDE_CODE | CONTEXT_IGNORE_SECURITY);
+				} else service_context = createPackageContext(component.getPackageName(), CONTEXT_INCLUDE_CODE | CONTEXT_IGNORE_SECURITY);
 				final Class<?> service_class = service_context.getClassLoader().loadClass(component.getClassName());
 				service = (Service) service_class.newInstance();
 				//noinspection JavaReflectionMemberAccess
