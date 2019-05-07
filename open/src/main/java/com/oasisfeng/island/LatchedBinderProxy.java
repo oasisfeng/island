f464713e5721977a411d7af6105c4feb1ebd1fb4
oasisfeng <git@id.oasisfeng.com>
2019-05-07 10:41:50 +0800

    UPDATE: Support cross-profile binding in open API container.

--- open/src/main/java/com/oasisfeng/island/LatchedBinderProxy.java
+++ open/src/main/java/com/oasisfeng/island/LatchedBinderProxy.java
@@ -5,9 +5,12 @@ import android.os.DeadObjectException;
 import android.os.IBinder;
 import android.os.IInterface;
 import android.os.Parcel;
+import android.os.Parcelable;
 import android.os.RemoteException;
+import android.util.Log;
 
 import java.io.FileDescriptor;
+import java.util.Objects;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.ExecutionException;
 
@@ -21,14 +24,7 @@ import static android.os.Build.VERSION_CODES.M;
 /**
  * Created by Oasis on 2019-5-3.
  */
-public class LatchedBinderProxy extends Binder {
-
-	void setDelegate(final IBinder delegate) { mDelegate.complete(delegate); }
-
-	void cancel(final @Nullable Throwable e) {
-		if (e != null) mDelegate.completeExceptionally(e);
-		else mDelegate.cancel(false);
-	}
+public class LatchedBinderProxy extends Binder implements Parcelable {
 
 	@Override protected boolean onTransact(final int code, @NonNull final Parcel data, @Nullable final Parcel reply, final int flags) throws RemoteException {
 		return delegateThrows().transact(code, data, reply, flags);
@@ -36,7 +32,7 @@ public class LatchedBinderProxy extends Binder {
 
 	private IBinder delegateThrows() throws RemoteException {
 		try {
-			return mDelegate.get();
+			return Objects.requireNonNull(mDelegate).get();
 		} catch (final ExecutionException e) {
 			final Throwable cause = e.getCause();
 			throw cause instanceof RuntimeException ? (RuntimeException) cause : new IllegalStateException(cause);
@@ -72,5 +68,69 @@ public class LatchedBinderProxy extends Binder {
 	@Override public boolean isBinderAlive() { return delegate().isBinderAlive(); }
 	@Override public @Nullable IInterface queryLocalInterface(@NonNull final String descriptor) { return delegate().queryLocalInterface(descriptor); }
 
-	private final CompletableFuture<IBinder> mDelegate = new CompletableFuture<>();
+	void setDelegate(final IBinder delegate) {
+		if (mDelegate != null) {
+			mDelegate.complete(delegate);
+			return;
+		}
+		final Parcel data = Parcel.obtain();
+		try {
+			data.writeNoException();
+			data.writeStrongBinder(delegate);
+			mRemote.transact(FIRST_CALL_TRANSACTION, data, null, FLAG_ONEWAY);
+		} catch (final RemoteException e) {
+			Log.e(TAG, "Error passing binder delegate", e);
+		} finally {
+			data.recycle();
+		}
+	}
+
+	void cancel(final @Nullable Exception e) {
+		if (mDelegate != null) {
+			if (e != null) mDelegate.completeExceptionally(e);
+			else mDelegate.cancel(false);
+		} else {
+			final Parcel data = Parcel.obtain();
+			try {
+				if (e == null) {
+					data.writeNoException();
+					data.writeStrongBinder(null);
+				} else data.writeException(e);
+				mRemote.transact(FIRST_CALL_TRANSACTION, data, null, FLAG_ONEWAY);
+			} catch (final RemoteException re) {
+				Log.e(TAG, "Error passing cancellation: " + e, re);
+			} finally {
+				data.recycle();
+			}
+		}
+	}
+
+	@Override public void writeToParcel(final Parcel dest, final int flags) {
+		dest.writeStrongBinder(mRemote = new Binder() { @Override protected boolean onTransact(final int code, @NonNull final Parcel data, @Nullable final Parcel reply, final int flags) throws RemoteException {
+			if (code != FIRST_CALL_TRANSACTION) return super.onTransact(code, data, reply, flags);
+			try {
+				data.readException();
+				final IBinder delegate = data.readStrongBinder();
+				if (delegate != null) setDelegate(delegate);
+				else cancel(null);
+			} catch (final Exception e) {
+				cancel(e);
+			}
+			return true;
+		}});
+	}
+	@Override public int describeContents() { return 0; }
+	private LatchedBinderProxy(final Parcel in) { mRemote = in.readStrongBinder(); mDelegate = null; }
+
+	LatchedBinderProxy() { mDelegate = new CompletableFuture<>(); mRemote = null; }
+
+	private final @Nullable CompletableFuture<IBinder> mDelegate;
+	private IBinder mRemote;
+
+	public static final Creator<LatchedBinderProxy> CREATOR = new Creator<LatchedBinderProxy>() {
+		@Override public LatchedBinderProxy createFromParcel(final Parcel in) { return new LatchedBinderProxy(in); }
+		@Override public LatchedBinderProxy[] newArray(final int size) { return new LatchedBinderProxy[size]; }
+	};
+
+	private static final String TAG = "Island.LBP";
 }
