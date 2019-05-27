b772e6852aae3612425a4200a2378b71747bbf54
oasisfeng <git@id.oasisfeng.com>
2019-05-27 22:27:03 +0800

    FIX: Crash for stock app details settings triggered by app settings action on Android pre-O.

--- installer/src/main/java/com/oasisfeng/island/installer/AppInfoForwarderActivity.java
+++ installer/src/main/java/com/oasisfeng/island/installer/AppInfoForwarderActivity.java
@@ -48,22 +48,28 @@ public class AppInfoForwarderActivity extends CallerAwareActivity {
 		Intent app_detail = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.fromParts("package", pkg, null));
 		final List<ResolveInfo> app_detail_resolves = pm.queryIntentActivities(app_detail, 0);    // Must before forceForwardingToIsland()
 		final ResolveInfo app_detail_resolve = app_detail_resolves == null || app_detail_resolves.isEmpty() ? null : app_detail_resolves.get(0);
-		final boolean caller_is_settings = app_detail_resolve != null && app_detail_resolve.activityInfo.packageName.equals(caller);
-		if (! caller_is_settings && ! UserHandles.MY_USER_HANDLE.equals(user)) {
-			if (user.equals(Users.profile) && app_detail_resolve != null) {
-				app_detail.setComponent(ActivityShuttle.selectForwarder(app_detail_resolves));	// ACTION_APPLICATION_DETAILS_SETTINGS was added to forwarding by IslandProvisioning
-				// Use mainland resolve to replace the misleading forwarding-resolved "Switch to work profile".
+		boolean caller_is_settings = false;
+		if (app_detail_resolve != null) {
+			caller_is_settings = app_detail_resolve.activityInfo.packageName.equals(caller);
+			if (! caller_is_settings && ! UserHandles.MY_USER_HANDLE.equals(user)) {
+				if (user.equals(Users.profile)) app_detail.setComponent(ActivityShuttle.selectForwarder(app_detail_resolves));	// ACTION_APPLICATION_DETAILS_SETTINGS was added to forwarding by IslandProvisioning
+				else app_detail = null;    // TODO: Not the default managed profile, use LauncherApps.startAppDetailsActivity().
+			} else ActivityShuttle.forceNeverForwarding(pm, app_detail);
+
+			if (SDK_INT < O && ! caller_is_settings && app_detail != null && ! hasNonForwardingResolves(pm, target))
+				return app_detail;		// Simulate EXTRA_AUTO_LAUNCH_SINGLE_CHOICE on Android pre-O.
+		} else app_detail = null;
+
+		final Intent chooser = Intent.createChooser(target, getString(R.string.app_info_forwarder_title)).addFlags(FLAG_ACTIVITY_FORWARD_RESULT);
+		if (SDK_INT >= O) chooser.putExtra(IntentCompat.EXTRA_AUTO_LAUNCH_SINGLE_CHOICE, ! caller_is_settings);
+		if (app_detail != null && ! caller_is_settings) {
+			if (user.equals(Users.profile)) {	// Use mainland resolve to replace the misleading forwarding-resolved "Switch to work profile".
 				final ActivityInfo activity = app_detail_resolve.activityInfo;
 				app_detail = new LabeledIntent(app_detail, activity.packageName,
 						activity.labelRes != 0 ? activity.labelRes : activity.applicationInfo.labelRes, activity.getIconResource());
-			} else app_detail = null;    // TODO: Not the default managed profile, use LauncherApps.startAppDetailsActivity().
+			}
+			chooser.putExtra(EXTRA_INITIAL_INTENTS, new Parcelable[] { app_detail });
 		}
-
-		if (SDK_INT < O && ! caller_is_settings && app_detail != null && ! hasNonForwardingResolves(pm, target))
-			return ActivityShuttle.forceNeverForwarding(pm, app_detail);    // Simulate EXTRA_AUTO_LAUNCH_SINGLE_CHOICE on Android pre-O.
-		final Intent chooser = Intent.createChooser(target, getString(R.string.app_info_forwarder_title)).addFlags(FLAG_ACTIVITY_FORWARD_RESULT);
-		if (SDK_INT >= O) chooser.putExtra(IntentCompat.EXTRA_AUTO_LAUNCH_SINGLE_CHOICE, ! caller_is_settings);
-		if (app_detail != null && ! caller_is_settings) chooser.putExtra(EXTRA_INITIAL_INTENTS, new Parcelable[] { app_detail });
 		return chooser;
 	}
 
