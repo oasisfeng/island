4fa283a1a31ffbf175495d82430664a9e3879c5e
oasisfeng <git@id.oasisfeng.com>
2020-06-21 23:59:51 +0800

    REVERT: "FIX: Remove the setting "Preserve App Ops", which is never checked actually. (always preserved)"
    Also refined the checked state refreshing.

--- mobile/src/main/java/com/oasisfeng/island/settings/GeneralPreferenceFragment.kt
+++ mobile/src/main/java/com/oasisfeng/island/settings/GeneralPreferenceFragment.kt
@@ -1,11 +1,15 @@
 package com.oasisfeng.island.settings
 
 import android.app.admin.DevicePolicyManager
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Build.VERSION.SDK_INT
 import android.os.Build.VERSION_CODES.N
 import android.os.Build.VERSION_CODES.P
 import android.os.Bundle
 import android.preference.TwoStatePreference
+import com.oasisfeng.android.ui.Dialogs
 import com.oasisfeng.island.appops.AppOpsCompat.GET_APP_OPS_STATS
 import com.oasisfeng.island.mobile.R
 import com.oasisfeng.island.util.DevicePolicies
@@ -36,5 +40,25 @@ class GeneralPreferenceFragment : SettingsActivity.SubPreferenceFragment(R.xml.p
                 true
             }
         }
+
+        setup<TwoStatePreference>(R.string.key_preserve_app_ops) {
+            if (SDK_INT < P) return@setup remove(this)
+            if (Permissions.has(activity, GET_APP_OPS_STATS)) lock(true)
+            else onChange { enabled -> false/* never toggle instantly */.also {
+                if (! enabled || refreshActivationStateForPreserveAppOps()) return@also
+                CoroutineScope(Dispatchers.Main).launch {
+                    val cmd = "pm grant " + Modules.MODULE_ENGINE + " " + GET_APP_OPS_STATS
+                    launch(Dispatchers.IO) { Shell.SU.run(cmd) }.join()
+
+                    if (refreshActivationStateForPreserveAppOps()) return@launch
+                    Dialogs.buildAlert(activity, null, getString(R.string.prompt_adb_app_ops_command) + "\n\n" + cmd)
+                            .withOkButton { refreshActivationStateForPreserveAppOps() }
+                            .setNeutralButton(R.string.action_copy) { _,_ ->
+                                val cm = activity?.getSystemService(Context.CLIPBOARD_SERVICE) as? ClipboardManager
+                                cm?.setPrimaryClip(ClipData.newPlainText(null, cmd))
+                            }.show() }}}}
     }
+
+    private fun TwoStatePreference.refreshActivationStateForPreserveAppOps()
+            = Permissions.has(activity, GET_APP_OPS_STATS).also { if (it) lock(true) }
 }
