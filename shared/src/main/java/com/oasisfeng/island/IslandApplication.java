40dc1b33098b592e5009b98ba2983a11b34def59
oasisfeng <git@id.oasisfeng.com>
2020-10-17 16:45:27 +0800

    UPDATE: Migrate Fabric Crashlytics to Firebase Crashlytics.
    FirebaseServiceProxy is removed, as Firebase Crashlytics is apparently working in China now.

--- shared/src/main/java/com/oasisfeng/island/IslandApplication.java
+++ shared/src/main/java/com/oasisfeng/island/IslandApplication.java
@@ -3,8 +3,6 @@ package com.oasisfeng.island;
 import android.app.Application;
 
 import com.oasisfeng.island.analytics.CrashReport;
-import com.oasisfeng.island.firebase.FirebaseServiceProxy;
-import com.oasisfeng.island.shared.R;
 
 /**
  * For singleton instance purpose only.
@@ -21,12 +19,6 @@ public class IslandApplication extends Application {
 		if (sInstance != null) throw new IllegalStateException("Already initialized");
 		sInstance = this;
 		CrashReport.initCrashHandler();
-	}
-
-	@Override public void onCreate() {
-		super.onCreate();
-		final String firebase_proxy_host = getString(R.string.firebase_proxy_host);
-		if (! firebase_proxy_host.isEmpty()) FirebaseServiceProxy.initialize(firebase_proxy_host);
 	}
 
 	private static IslandApplication sInstance;
