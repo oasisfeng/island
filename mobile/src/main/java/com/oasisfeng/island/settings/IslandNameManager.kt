4812f05c629f20ecb01100949a1843200d2b8380
oasisfeng <git@id.oasisfeng.com>
2020-10-17 16:32:47 +0800

    FIX: Never use specialized Island name if only one Island is available.
    New specialized name is introduced for the first Island to replace its generalized name.

--- mobile/src/main/java/com/oasisfeng/island/settings/IslandNameManager.kt
+++ mobile/src/main/java/com/oasisfeng/island/settings/IslandNameManager.kt
@@ -4,24 +4,21 @@ import android.app.admin.DevicePolicyManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.UserHandle
-import android.util.ArrayMap
 import com.oasisfeng.island.mobile.R
 import com.oasisfeng.island.shuttle.Shuttle
 import com.oasisfeng.island.util.*
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 object IslandNameManager {
 
 	@JvmStatic fun getAllNames(context: Context): Map<UserHandle, String> {
 		val profiles = Users.getProfilesManagedByIsland()
-		return ArrayMap<UserHandle, String>(profiles.size).apply {
-			if (profiles.isNotEmpty()) getStore(context).also {
-				profiles.forEach { profile ->
-					val saved = getStore(context).getString(buildIslandNameKey(context, profile.toId()), null)
-					put(profile, saved ?: getDefaultName(context, profile)) }}}
+		return when(profiles.size) {
+			0 -> emptyMap()
+			1 -> mapOf(Pair(profiles[0], context.getString(R.string.default_island_name)))
+			else -> getStore(context).let { store -> profiles.associateWith { profile ->
+				store.getString(buildIslandNameKey(context, profile.toId()), null) ?: getDefaultName(context, profile) }}}
 	}
 
 	fun getDefaultName(context: Context, profile: UserHandle = Users.current()) = when (val profileId = profile.toId()) {
@@ -29,7 +26,7 @@ object IslandNameManager {
 		11   -> context.getString(R.string.default_island1_name)
 		12   -> context.getString(R.string.default_island2_name)
 		13   -> context.getString(R.string.default_island3_name)
-		else -> context.getString(R.string.default_island_name, profileId)
+		else -> context.getString(R.string.default_islandN_name, profileId)
 	}
 
 	@ProfileUser fun syncNameToOwnerUser(context: Context, name: String)    // TODO: syncNameToParentProfile() with proper "parent".
