71fdef72858b37f5e1b10d39a55e83937f38ee83
oasisfeng <git@id.oasisfeng.com>
2022-02-17 20:09:41 +0800

    UPDATE: Use code-local default values for Remote Config.

--- shared/src/main/java/com/oasisfeng/island/Config.java
+++ shared/src/main/java/com/oasisfeng/island/Config.java
@@ -1,5 +1,7 @@
 package com.oasisfeng.island;
 
+import static com.google.firebase.remoteconfig.internal.ConfigFetchHandler.DEFAULT_MINIMUM_FETCH_INTERVAL_IN_SECONDS;
+
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigValue;
@@ -8,44 +10,44 @@ import com.oasisfeng.island.shared.BuildConfig;
 import com.oasisfeng.island.shared.R;
 
 /**
- * Remotely configurable values, (default values are defined in config_defaults.xml)
+ * Remotely configurable values
  *
  * Created by Oasis on 2016/5/26.
  */
 public enum Config {
 
-	/* All keys must be consistent with config_defaults.xml */
-	IS_REMOTE("is_remote"),
-	URL_PLAY_ALPHA("url_play_alpha"),
-	URL_FAQ("url_faq"),
-	URL_SETUP("url_setup"),
-	URL_SETUP_MANAGED_MAINLAND("url_setup_god_mode"),
-	URL_SETUP_TROUBLESHOOTING("url_setup_trouble"),
-	PERMISSION_REQUEST_ALLOWED_APPS("permission_allowed_apps");
+	IS_REMOTE("is_remote", ""),
+	URL_PLAY_ALPHA("url_play_alpha", "https://groups.google.com/g/islandroid"),
+	URL_FAQ("url_faq", "https://island.oasisfeng.com/faq"),
+	URL_SETUP("url_setup", "https://island.oasisfeng.com/setup"),
+	URL_SETUP_MANAGED_MAINLAND("url_setup_god_mode", "https://island.oasisfeng.com/setup#activate-managed-mainland"),
+	URL_SETUP_TROUBLESHOOTING("url_setup_trouble", "https://island.oasisfeng.com/faq"),
+	PERMISSION_REQUEST_ALLOWED_APPS("permission_allowed_apps", "com.oasisfeng.greenify,com.oasisfeng.nevo");
 
 	public String get() {
 		final FirebaseRemoteConfig config = FirebaseRemoteConfig.getInstance();
-		config.activateFetched();
-		return config.getString(key);
+		config.activate();
+		final String value = config.getString(key);
+		return ! value.isEmpty() ? value : defaultValue;
 	}
 
 	public static boolean isRemote() {
 		final FirebaseRemoteConfig config = FirebaseRemoteConfig.getInstance();
-		config.activateFetched();
+		config.activate();
 		final FirebaseRemoteConfigValue value = config.getValue(IS_REMOTE.key);
 		return value.getSource() == FirebaseRemoteConfig.VALUE_SOURCE_REMOTE;
 	}
 
-	Config(final String key) { this.key = key; }
+	Config(final String key, final String defaultValue) { this.key = key; this.defaultValue = defaultValue; }
 
 	private final String key;
+	private final String defaultValue;
 
-	static {
+	static { //noinspection ResultOfMethodCallIgnored
 		FirebaseWrapper.init();
-		final FirebaseRemoteConfigSettings settings = new FirebaseRemoteConfigSettings.Builder().setDeveloperModeEnabled(BuildConfig.DEBUG).build();
 		final FirebaseRemoteConfig config = FirebaseRemoteConfig.getInstance();
-		config.setConfigSettings(settings);
-		config.setDefaults(R.xml.config_defaults);
-		config.fetch();
+		final FirebaseRemoteConfigSettings.Builder settings = new FirebaseRemoteConfigSettings.Builder()
+				.setMinimumFetchIntervalInSeconds(BuildConfig.DEBUG ? 30 : DEFAULT_MINIMUM_FETCH_INTERVAL_IN_SECONDS);
+		config.setConfigSettingsAsync(settings.build()).addOnCompleteListener(task -> config.fetch());
 	}
 }
